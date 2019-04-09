64ccb20ffed70eb15de102905afd166738ce552d
wanglai1111 <862581476@qq.com>
2019-04-09 15:16:25 +0800

    这是添加到具体商品信息及添加到购物车

--- js/goodsDetail.js
+++ js/goodsDetail.js
@@ -4,40 +4,69 @@ $(function () {
     deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
   });
   //声明个全局变量
-  var info ={
-      cat_id:'',
-      goods_id:'',
-      goods_name:'',
-      goods_number:'',
-      goods_price:'',
-      goods_small_logo:'',
-      goods_weight:''
+  var info = {
+    cat_id: '',
+    goods_id: '',
+    goods_name: '',
+    goods_number: '',
+    goods_price: '',
+    goods_small_logo: '',
+    goods_weight: ''
   }
- //发送请求
- $.ajax({
-   type:'get',
-   url: 'goods/detail',
-   data: $.getParameter(location.search),
-   dataType:'json',
-   success:function (result) {
-     console.log(result);
+  //发送请求
+  $.ajax({
+    type: 'get',
+    url: 'goods/detail',
+    data: $.getParameter(location.search),
+    dataType: 'json',
+    success: function (result) {
+      console.log(result);
       //赋值给info
-        info.cat_id= 'result.cat_id'
-        info.goods_id= 'result.goods_id'
-        info.goods_name= 'result.goods_name'
-        info.goods_number= 'result.goods_number'
-        info.goods_price='result.goods_price'
-        info.goods_small_logo= 'result.goods_small_logo'
-        info.goods_weight= 'result.goods_weight'
-        var html = template('sliderTemp', result.data)
-        console.log(html);
-        //动态生成结构
-        $('.mui-scroll').html(html)
-         mui('.mui-slider').slider({
-           interval: 2000
-         });
+      info.cat_id = 'result.cat_id'
+      info.goods_id = 'result.goods_id'
+      info.goods_name = 'result.goods_name'
+      info.goods_number = 'result.goods_number'
+      info.goods_price = 'result.goods_price'
+      info.goods_small_logo = 'result.goods_small_logo'
+      info.goods_weight = 'result.goods_weight'
+      var html = template('sliderTemp', result.data)
+      console.log(html);
+      //动态生成结构
+      $('.mui-scroll').html(html)
+      mui('.mui-slider').slider({
+        interval: 2000
+      });
+    }
+  })
+//点击购物车按钮 添加商品到购物车
+ $('.btn-addCart').on('tap',function(){
+   console.log(111);
+   //声明个变量存储数据 采用临时存储对象sessionStorage存储
+   var mytoken = sessionStorage.getItem('pyg_token')
+   console.log(mytoken);
+   //用户操作界面成功一次就存在token，如果没有token 需要登陆
+   if (!mytoken) {
+     //登陆
+     location.href ='./login.html?redirectUrl='+escape(location.href)
+     //否则 不需要登陆 直接发送请求 从存储对象中获取数据
+   }else{
+     $.ajax({
+       type:'post',
+       url:'my/cart/add',
+       data:JSON.stringify(info),
+       dataType:'json',
+       success:function (result) {
+         //整个间隔时间如果太长需要重新登陆
+         if (result.meta.status == 401) {
+            location.href = './login.html?redirectUrl=' + escape(location.href)
+         }else{
+           //否则出现提示成功;
+           alert('ok')
+           
+           
+         }
+       }
+     })
    }
  })
-
-
 })
---
+++ js/login.js
@@ -0,0 +1,54 @@
+$(function () {
+  //为确认按钮添加点击事件，点击触发事件 收集用户数据
+  $('.mui-btn-primary').on('tap',function () {
+    //声明个变量收集用户数据
+    var obj ={
+      username:'',
+      password:''
+    }
+    //获取手机号码和密码并赋值
+    obj.username = $('.username').val()
+    obj.password = $('.password').val()
+    //正则验证手机号码
+    if (!/^1[3-9]\d{9}$/.test(obj.username)) {
+      //号码不符合则出现提示消息框
+      mui.toast('手机号码输入不正确')
+      return false
+    }
+    //密码验证
+    if (obj.password.length<6) {
+      mui.toast('请输入正确的密码')
+      return false
+    }
+    //发送ajax请求，根基token跳转页面
+    // console.log(obj);
+    
+    $.ajax({
+      type:'post',
+      url:'login',
+      data:obj,
+      dataType:'json',
+      success:function (result) {
+        console.log(result);
+        //如果登入成功
+        if (result.meta.status == 200) {
+
+          //存储当前token
+          sessionStorage.setItem('pyg_token',result.data.token)
+          //对url进行编码
+          var re = $.getParameter(location.search).redirectUrl
+          //判断是否跳转
+          if (re) {
+            //满足条件 则对url进行解码
+            location.href = unescape(re)
+          }else{
+            location.href='/index.html'
+          }
+          //登入失败
+        }else{
+           mui.toast(result.meta.msg)
+        }
+      }
+    })
+  })
+})
